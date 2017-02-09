f5b40a76bb79fde9bcc1832d3fd48cb82f5e41af
Anna Kimtis <Anna@Annas-MBP.verizon.net>
2017-02-09 15:45:28 -0500

    added interviewquestions.js

---
+++ interview_quesitons.js
@@ -0,0 +1,41 @@
+//interview questions
+// Level 1:
+
+// Write a function named greatThanThree that takes in two numbers and returns true if they are both greater than 3 and false if not.
+
+function greatThanThree (a, b){
+  if (a > 3 && b > 3) 
+    {return true;}
+  else {return false};
+}
+
+// Write a function named aOrB that takes in a letter and if it is a or b then return true otherwise return false.
+
+function aORB (letter){
+  if (letter == "a" or letter == "b")
+    {return true;}
+  else {return false;}
+}
+
+// Write a function named greatThanTwo that returns true if the number is 2 and false otherwise
+
+function greatThanTwo (number){
+  if (number == 2){
+    return true;
+  }
+  else{ return false;}
+}
+
+// Level 2:
+
+// eventually you want to be able to do each of these questions on paper in 5 minutes or less
+
+// Write a function named printEvenBetween that takes in two numbers and will return an array of all the even numbers between the given two numbers.
+
+// Write a function named printBetween that takes in two arguments. Assume they are integers. This function will return an array of the numbers between those two arguments.
+
+// Level 3:
+
+// eventually you want to be able to do this question on paper in 10 minutes or less
+
+// Write a function named fib that takes an argument n. Assume that n is a number. And the function returns an array of n numbers of the fibonacci sequence.
